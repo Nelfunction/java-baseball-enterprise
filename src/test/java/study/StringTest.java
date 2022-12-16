73a0dcf9ed82114c844c86721602d78d5baea471
Nelfunction <oat641@gmail.com>
2022-12-16 15:24:10 +0900

    test: String class req2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -12,11 +12,22 @@ public class StringTest {
     }
 
     @Test
+    // req1
     void split() {
         String[] actual0 = "1,2".split(",");
         assertThat(actual0).containsExactly("1","2");
 
         String[] actual1 = "1".split(",");
         assertThat(actual1).containsExactly("1");
+    }
+
+    @Test
+    // req2
+    void substring() {
+        String given = "(1,2)";
+
+        String when = given.substring(1, 4);
+
+        assertThat(when).isEqualTo("1,2");
     }
 }
