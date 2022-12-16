22fc8ec0c15adbec32050725d45573f9baf004d8
Nelfunction <oat641@gmail.com>
2022-12-16 16:13:49 +0900

    test: Set class req3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,9 +2,11 @@ package study;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.*;
@@ -32,5 +34,12 @@ public class SetTest {
     @DisplayName("요구사항 2")
     void contains(int i) {
         assertTrue(numbers.contains(i));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("요구사항 3")
+    void contains_csvSource(int i, boolean b) {
+        assertEquals(numbers.contains(i), b);
     }
 }
