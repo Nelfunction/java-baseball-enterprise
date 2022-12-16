0ca6228d9bd7aaf368cd33b9fd56f17c1b1ce6ed
Nelfunction <oat641@gmail.com>
2022-12-16 15:13:02 +0900

    test: String class req1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,12 +2,21 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual0 = "1,2".split(",");
+        assertThat(actual0).containsExactly("1","2");
+
+        String[] actual1 = "1".split(",");
+        assertThat(actual1).containsExactly("1");
     }
 }
